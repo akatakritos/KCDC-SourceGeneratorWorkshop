46e9411204fa9ecd1b3a5e2a88c9dd6d3d81ef39
Matt Burke <matt.burke@abouthealthcare.com>
2024-06-26 11:35:02 -0500

    Package as nuget

---
+++ AutoProperty.NugetTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using AutoProperty.Generator;
+using AutoProperty.NugetTest;
+
+[assembly: AutoPropertyAttribute(typeof(IModel))]
+
+namespace AutoProperty.NugetTest
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var model = new Models()
+            {
+                Id = 2
+            };
+            Console.WriteLine(model);
+        }
+    }
+
+
+    public interface IModel
+    {
+        public int Id { get; set; }
+    }
+
+    public partial class Models : IModel
+    {
+    }
+}
