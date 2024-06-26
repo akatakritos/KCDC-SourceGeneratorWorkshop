460441e4a5dbafb31ec069b9f14a610819c9aeff
Matt Burke <matt.burke@abouthealthcare.com>
2024-06-26 10:48:37 -0500

    Generates missing interface methods

--- AutoProperty.Sample/Book.cs
+++ AutoProperty.Sample/Book.cs
@@ -1,16 +1,36 @@
 namespace AutoProperty.Sample;
 
 
+public interface IHasId
+{
+    public int Id { get; set; }
+}
+
+public interface IHasTitle
+{
+    public string Title { get; set; }
+}
+
+public interface IHasActiveFlag
+{
+    public bool IsActive { get; set; }
+}
+
 public interface IAuditMetadata
 {
     DateTimeOffset LastUpdated { get; set; }
+
+    bool IsEditable { get; }
 }
 
-public partial class Book : IAuditMetadata
+public partial class Book : IHasId, IHasTitle, IHasActiveFlag, IAuditMetadata
 {
     public required string Title { get; set; }
 
     public required string Author { get; set; }
+}
 
-    public DateTimeOffset LastUpdated { get; set; }
+public partial class Author : IHasId, IHasActiveFlag, IAuditMetadata
+{
+    public required string Name { get; set; }
 }
